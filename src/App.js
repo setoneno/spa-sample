40e7db5d4ab4db013e988756acb3e9338bbdc497
EC2 Default User <ec2-user@ip-172-31-71-215.ec2.internal>
2019-09-15 09:18:59 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function ChangeButton(props) {
+  return (
+    <a href={props.link} onClick={() => props.onClick()}>
+      {props.value}
+    </a>
+  );
+}
+
+class Body extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: <div>main</div> };
+  }
+
+  showPage1() {
+    this.setState({ value: (<div>page1 <img src={`${process.env.PUBLIC_URL}/img/aaa.pdf`}/></div>) });
+  }
+
+  showPage2() {
+    this.setState({ value: (<div>page2 <img src={`${process.env.PUBLIC_URL}/img/bbb.pdf`}/></div>) });
+  }
+
+  showPage3() {
+    this.setState({ value: (<div>page3 <img src={`${process.env.PUBLIC_URL}/img/ccc.pdf`}/></div>) });
+  }
+  
+
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>
+            <ChangeButton link="/" value="main" onClick={() => this.state()} />
+          </li>
+          <li>
+            <ChangeButton link="#1" value="page1" onClick={() => this.showPage1()} />
+          </li>
+          <li>
+            <ChangeButton link="#2" value="page2" onClick={() => this.showPage2()} />
+          </li>
+          <li>
+            <ChangeButton link="#3" value="page3" onClick={() => this.showPage3()} />
+          </li>
+        </ul>
+        {this.state.value}
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Body />
       </div>
     );
   }
